b19f99c6d07787bd3e3ab79cee02ca93c40cd2fb
eternalfever <48808930+eternalfever@users.noreply.github.com>
2020-05-23 03:44:15 +0300

    Update myAlbums.js

--- static/scripts/myAlbums.js
+++ static/scripts/myAlbums.js
@@ -1,12 +1,12 @@
 $("div[class^='alb-']").hover(
     function() {
         var alb_class = $(this).attr("class");
-        var str = '.' + alb_class + '> .img_wrap > .alb_name > .settings';
+        var str = '.' + alb_class + '> .wrap_alb > .alb_name > .settings';
         $(str).css('visibility', 'unset');
     },
 
     function() {
         var alb_class = $(this).attr("class");
-        var str = '.' + alb_class + '> .img_wrap > .alb_name > .settings';
+        var str = '.' + alb_class + '> .wrap_alb > .alb_name > .settings';
         $(str).css('visibility', 'hidden');
     });
